62f9ca17f7cb032dd6e36fa4d11e81e4ab971399
Marcos Garcia <marcosxtk130@hotmail.com>
2022-08-29 17:50:59 -0300

    getWeather now is an async function that only handles the fetch, and added addWeatherInfo function that handles the dom

--- client-side/public/js/main.js
+++ client-side/public/js/main.js
@@ -1,32 +1,37 @@
-document.getElementById("inputBtn").addEventListener("click", getWeather);
+const form = document.querySelector('form');
+const element = (element) => document.getElementById(element);
 
-function getWeather() {
-    const getElement = (el) => document.getElementById(el);
-    let location_inputed = getElement("inputTxt").value;
-    location_inputed.trim();
-    fetch(`https://api-weather-marcosag.herokuapp.com/${location_inputed}`)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          let fourTofourHours = [0, 4, 8, 12, 16, 20];
-          getElement("city").innerText = data.location.name;
-          getElement("region").innerText = data.location.region;
-          getElement("date").innerText = new Date(data.location.localtime).toLocaleDateString("en-US", {weekday: "long", month: "long", day: "2-digit", hour:"2-digit", minute:"2-digit"});
-          getElement("temperature").innerText = `${data.current["temp_c"]}°C`; 
-          getElement("more-temp").innerHTML = `${data.forecast.forecastday[0].day.maxtemp_c}° / ${data.forecast.forecastday[0].day.mintemp_c}° | Feels like: ${data.current["feelslike_c"]}°`; // work on it
-          getElement("img-temp").src = `http:${data.current.condition.icon}`;
-          getElement("curent-description").innerText = data.current.condition.text;
-          getElement("last-updated").innerText = `Last updated: ${new Date(data.current["last_updated"]).toLocaleDateString("en-US", {weekday: "long", day: "2-digit", hour:"2-digit", minute:"2-digit"})}`;
-          for (let e = 0; e <= 5; e++){
-            if (e > 0) getElement(`hour${e}`).style = "border-left: 1px solid;";
-            getElement(`forecast_hour${e}`).innerText = new Date(data.forecast.forecastday[0].hour[fourTofourHours[e]].time).toLocaleTimeString([], {hour: "2-digit"});
-            getElement(`forecast-img${e}`).src = `http:${data.forecast.forecastday[0].hour[fourTofourHours[e]].condition.icon}`;
-            getElement(`forecast-temperature${e}`).innerHTML = `${data.forecast.forecastday[0].hour[fourTofourHours[e]].temp_c.toFixed(1)}°`;
-          }
-          getElement('data-container').classList.remove('none')
-          getElement('main').classList.add('main-container')
-        })
-        .catch((error) => {
-          console.log(`Error ${error}`);
-        });
+form.addEventListener("submit", getWeather);
+
+async function getWeather(event) {
+  event.preventDefault()
+  const location_inputed = element("inputTxt").value;
+  location_inputed.trim();
+  try {
+    const weather = await fetch(`https://api-weather-marcosag.herokuapp.com/${location_inputed}`)
+    const data = await weather.json();
+    addWeatherInfo(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function addWeatherInfo(data) {
+  console.log(data);
+  const fourTofourHours = [0, 4, 8, 12, 16, 20];
+  element("city").innerText = data.location.name;
+  element("region").innerText = data.location.region;
+  element("date").innerText = new Date(data.location.localtime).toLocaleDateString("en-US", { weekday: "long", month: "long", day: "2-digit", hour: "2-digit", minute: "2-digit" });
+  element("temperature").innerText = `${data.current["temp_c"]}°C`;
+  element("more-temp").innerHTML = `${data.forecast.forecastday[0].day.maxtemp_c}° / ${data.forecast.forecastday[0].day.mintemp_c}° | Feels like: ${data.current["feelslike_c"]}°`;
+  element("img-temp").src = `http:${data.current.condition.icon}`;
+  element("curent-description").innerText = data.current.condition.text;
+  element("last-updated").innerText = `Last updated: ${new Date(data.current["last_updated"]).toLocaleDateString("en-US", { weekday: "long", day: "2-digit", hour: "2-digit", minute: "2-digit" })}`;
+  for (let e = 0; e <= 5; e++) {
+    element(`forecast_hour${e}`).innerText = new Date(data.forecast.forecastday[0].hour[fourTofourHours[e]].time).toLocaleTimeString([], { hour: "2-digit" });
+    element(`forecast-img${e}`).src = `http:${data.forecast.forecastday[0].hour[fourTofourHours[e]].condition.icon}`;
+    element(`forecast-temperature${e}`).innerHTML = `${data.forecast.forecastday[0].hour[fourTofourHours[e]].temp_c.toFixed(1)}°`;
+  }
+  element('data-container').classList.remove('none')
+  element('main').classList.add('main-container')
 }
