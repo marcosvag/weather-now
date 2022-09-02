793907cdfd4461e89476fc17f4b1eaf4dc65cdbf
Marcos Garcia <marcosxtk130@hotmail.com>
2022-09-02 02:16:58 -0300

    small corrections

--- client-side/public/js/main.js
+++ client-side/public/js/main.js
@@ -18,20 +18,32 @@ async function getWeather(event) {
 
 function addWeatherInfo(data) {
   console.log(data);
-  const fourTofourHours = [0, 4, 8, 12, 16, 20];
-  element("city").innerText = data.location.name;
-  element("region").innerText = data.location.region;
-  element("date").innerText = new Date(data.location.localtime).toLocaleDateString("en-US", { weekday: "long", month: "long", day: "2-digit", hour: "2-digit", minute: "2-digit" });
-  element("temperature").innerText = `${data.current["temp_c"]}°C`;
-  element("more-temp").innerHTML = `${data.forecast.forecastday[0].day.maxtemp_c}° / ${data.forecast.forecastday[0].day.mintemp_c}° | Feels like: ${data.current["feelslike_c"]}°`;
-  element("img-temp").src = `http:${data.current.condition.icon}`;
-  element("curent-description").innerText = data.current.condition.text;
-  element("last-updated").innerText = `Last updated: ${new Date(data.current["last_updated"]).toLocaleDateString("en-US", { weekday: "long", day: "2-digit", hour: "2-digit", minute: "2-digit" })}`;
+  element("city").innerText = data["city"];
+  element("region").innerText = data["region"];
+  element("date").innerText = new Date(data["localtime"]).toLocaleDateString(undefined, { weekday: "long", month: "long", day: "2-digit", hour: "2-digit", minute: "2-digit" });
+  element("temperature").innerText = `${data["temp_c"]}°C`;
+  element("more-temp").innerHTML = `${data["maxtemp_c"]}° / ${data["mintemp_c"]}° | Feels like: ${data["feelslike_c"]}°`;
+  element("img-temp").src = `http:${data["current_img"]}`;
+  element("curent-description").innerText = data["current_text"];
+  element("last-updated").innerText = `Last updated: ${new Date(data["last_updated"]).toLocaleDateString(undefined, { weekday: "long", day: "2-digit", hour: "2-digit", minute: "2-digit" })}`;
+
+  let now = new Date().toLocaleTimeString([],{hour: "2-digit"});
+  let futureTime = Number(new Date().toLocaleTimeString([],{hour12: false, hour: "2-digit"}));
+  let max = now.length > 2 ? 12 : 24;
+  let day = 'today';
+
   for (let e = 0; e <= 5; e++) {
-    element(`forecast_hour${e}`).innerText = new Date(data.forecast.forecastday[0].hour[fourTofourHours[e]].time).toLocaleTimeString([], { hour: "2-digit" });
-    element(`forecast-img${e}`).src = `http:${data.forecast.forecastday[0].hour[fourTofourHours[e]].condition.icon}`;
-    element(`forecast-temperature${e}`).innerHTML = `${data.forecast.forecastday[0].hour[fourTofourHours[e]].temp_c.toFixed(1)}°`;
+    futureTime  += 4;
+    if(futureTime > 23) {
+      futureTime -= 24;
+      day = 'tomorrow';
+    }
+    let futureDisplayTime = `${max == 24 ? futureTime : futureTime > 12 ? futureTime - 12 : futureTime}${max == 24 ?  'h' : futureTime > 12 ? ' PM' : " AM"}`;
+    element(`forecast_hour${e}`).innerText = futureDisplayTime;
+    element(`forecast-img${e}`).src = `http:${data["forecast_hourly"][day][futureTime]["forecast_img"]}`;
+    element(`forecast-temperature${e}`).innerHTML = `${data["forecast_hourly"][day][futureTime]["forecast_temp_c"].toFixed(1)}°`;
   }
+  
   element('data-container').classList.remove('none');
   element('main').classList.add('main-container');
 }
